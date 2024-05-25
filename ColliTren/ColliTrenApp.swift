066e9eb688fef4424fe95f0ef4e11f148debc2c3
sfwgtlab <81831447+sfwgtlab@users.noreply.github.com>
2024-05-24 20:03:35 -0600

    Initial Commit

---
+++ ColliTren/ColliTrenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColliTrenApp.swift
+//  ColliTren
+//
+//  Created by Eduardo Gutiérrez on 24/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ColliTrenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
